aa7721881bd8c0c020647072b332a54ddd0015c3
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-07-25 09:04:38 +0800

    update files info

---
+++ serialization/src/main/java/com/laibao/serialization/domain/User.java
@@ -0,0 +1,50 @@
+package com.laibao.serialization.domain;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+
+public class User{
+
+    private String name;
+
+    private String email;
+
+    private List<User> userList;
+
+    private Map<String, User> userMap;
+
+    public List<User> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(List<User> userList) {
+        this.userList = userList;
+    }
+
+    public Map<String, User> getUserMap() {
+        return userMap;
+    }
+
+    public void setUserMap(Map<String, User> userMap) {
+        this.userMap = userMap;
+    }
+
+    public String getName() {
+        System.out.println("-------------------"+name);
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- serialization/src/main/java/com/laibao/serialization/impl/XstreamSerializer.java
+++ serialization/src/main/java/com/laibao/serialization/impl/XstreamSerializer.java
@@ -1,6 +1,10 @@
 package com.laibao.serialization.impl;
 
 import com.laibao.serialization.ISerializer;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author laibao wang
@@ -8,12 +12,34 @@ import com.laibao.serialization.ISerializer;
  * @version 1.0
  */
 public class XstreamSerializer implements ISerializer{
+    private Logger logger = LoggerFactory.getLogger(XstreamSerializer.class);
+
+    private static final XStream xStream = new XStream(new DomDriver());
 
     public <T> byte[] serialize(T obj) {
-        return new byte[0];
+        if(obj == null) {
+            logger.error("obj is null");
+            throw new IllegalArgumentException("obj is null");
+        }
+        try{
+            return xStream.toXML(obj).getBytes();
+        }catch (Exception ex) {
+            logger.error("obj serialize failure!",ex.getMessage());
+            throw new RuntimeException("obj serialize failure!",ex);
+        }
     }
 
     public <T> T deserialize(byte[] data, Class<T> clazz) {
-        return null;
+        if (data == null) {
+            logger.error("argument error");
+            throw new IllegalArgumentException("argument error");
+        }
+        try{
+            String xml = new String(data);
+            return (T) xStream.fromXML(xml);
+        }catch (Exception ex) {
+            logger.error("data deserialize failure!",ex.getMessage());
+            throw new RuntimeException("data deserialize failure!",ex);
+        }
     }
 }

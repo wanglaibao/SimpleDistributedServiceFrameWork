9e0bee405b911ef9781f8150006e21e2b076743f
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-07-30 11:01:08 +0800

    update java files info

---
+++ springfactorybean/src/main/java/com/laibao/springrpc/facotybean/ConnectionProxyFactoryBean.java
@@ -0,0 +1,75 @@
+package com.laibao.springrpc.facotybean;
+
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.InitializingBean;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * @author laibao wang
+ * @date 2018-07-30
+ * @version 1.0
+ */
+public class ConnectionProxyFactoryBean implements FactoryBean<Connection>,InitializingBean{
+
+    private String jdbcDriver;
+
+    private String connectionUrl;
+
+    private String dbUserName;
+
+    private String dbPassword;
+
+    private Connection connection;
+
+    public Connection getObject() throws Exception {
+          /* Register jdbc driver class. */
+        Class.forName(jdbcDriver);
+		/* Get the jdbc Connection object. */
+        connection = DriverManager.getConnection(connectionUrl, dbUserName, dbPassword);
+        return connection;
+    }
+
+    public Class<?> getObjectType() {
+        return Connection.class;
+    }
+
+    public void afterPropertiesSet() throws Exception {
+        /* Register jdbc driver class. */
+        //Class.forName(jdbcDriver);
+		/* Get the jdbc Connection object. */
+        //connection = DriverManager.getConnection(connectionUrl, dbUserName, dbPassword);
+    }
+
+    public String getJdbcDriver() {
+        return jdbcDriver;
+    }
+
+    public void setJdbcDriver(String jdbcDriver) {
+        this.jdbcDriver = jdbcDriver;
+    }
+
+    public String getConnectionUrl() {
+        return connectionUrl;
+    }
+
+    public void setConnectionUrl(String connectionUrl) {
+        this.connectionUrl = connectionUrl;
+    }
+
+    public String getDbUserName() {
+        return dbUserName;
+    }
+
+    public void setDbUserName(String dbUserName) {
+        this.dbUserName = dbUserName;
+    }
+
+    public String getDbPassword() {
+        return dbPassword;
+    }
+
+    public void setDbPassword(String dbPassword) {
+        this.dbPassword = dbPassword;
+    }
+}
--- springfactorybean/src/test/java/com/laibao/springfactorybean/test/FactoryBeanTest.java
+++ springfactorybean/src/test/java/com/laibao/springfactorybean/test/FactoryBeanTest.java
@@ -8,6 +8,10 @@ import com.laibao.springrpc.domain.User;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 /**
  * @author laibao wang
  * @date 2018-07-26
@@ -48,5 +52,43 @@ public class FactoryBeanTest {
         Employee director =  applicationContext.getBean("director",Employee.class);
         System.out.println(JSON.toJSONString(director));;
         applicationContext.close();
+    }
+
+    @Test
+    public void testMySqlConnectionProxyFactoryBean() {
+        try {
+            ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext();
+            applicationContext.setConfigLocation("META-INF/spring/factorybean.xml");
+            applicationContext.refresh();
+            // Get mysql connection object.
+            Connection mysqlConn = applicationContext.getBean("mysqlDBConnection",Connection.class);
+            String mysqldbProductName = mysqlConn.getMetaData().getDatabaseProductName();
+            String mysqldbProductVersion = mysqlConn.getMetaData().getDatabaseProductVersion();
+            System.out.println("mysqldbProductName = " + mysqldbProductName + " , mysqldbProductVersion = " + mysqldbProductVersion);
+            applicationContext.close();
+        }catch(Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void testMySqlConnectio() throws Exception {
+        // JDBC driver name and database URL
+        final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+        final String DB_URL = "jdbc:mysql://116.62.138.41:3306/strategic_test";
+        //  Database credentials
+        final String USER = "dev";
+        final String PASS = "JlDV94BW!";
+        Connection conn = null;
+        Statement stmt = null;
+        //STEP 2: Register JDBC driver
+        Class.forName("com.mysql.jdbc.Driver");
+        //STEP 3: Open a connection
+        System.out.println("Connecting to database...");
+        conn = DriverManager.getConnection(DB_URL,USER,PASS);
+        //STEP 4: Execute a query
+        System.out.println("Creating statement...");
+        stmt = conn.createStatement();
     }
 }
